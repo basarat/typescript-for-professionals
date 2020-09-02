e8e6f65f5acff80e4df93944c41a296f99c51ebd
Basarat <basaratali@gmail.com>
2020-09-02 12:56:04 +1000

    primitive

---
+++ primitive/src/index.ts
@@ -0,0 +1,9 @@
+let isPresent: boolean = false;
+let magic: number = 66.6;
+let hello: string = 'world';
+
+let notDefined: undefined = undefined;
+let notPresent: null = null;
+
+let penta: symbol = Symbol('star');
+let biggy: bigint = 24n;
