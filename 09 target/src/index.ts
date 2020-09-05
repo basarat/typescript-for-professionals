a6798b6ce1a725139019ad8fb1197f685f8f0b09
Basarat <basaratali@gmail.com>
2020-09-06 09:03:21 +1000

    wip

--- 09 target/src/index.ts
+++ 09 target/src/index.ts
@@ -1,11 +1,9 @@
 class Animal {
   private name: string;
-
   constructor(name: string) {
     this.name = name;
   }
-
-  public move(distanceInMeters: number): void {
+  move(distanceInMeters: number) {
     console.log(`${this.name} moved ${distanceInMeters}m.`);
   }
 }
