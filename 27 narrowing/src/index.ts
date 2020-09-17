a641a90e0003a77cf2e9b9019386e94c6aa45030
Basarat <basaratali@gmail.com>
2020-09-17 12:58:37 +1000

    better

---
+++ 27 narrowing/src/index.ts
@@ -0,0 +1,22 @@
+type Square = {
+  size: number,
+};
+
+type Rectangle = {
+  width: number,
+  height: number,
+};
+
+type Shape = Square | Rectangle;
+
+function area(shape: Shape) {
+  if ('size' in shape) {
+    return shape.size * shape.size;
+  }
+  if ('width' in shape) {
+    return shape.width * shape.height;
+  }
+}
+
+area({ size: 2 });             // 4
+area({ width: 2, height: 3 }); // 6
