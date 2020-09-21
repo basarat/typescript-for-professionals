1668eec8485c85749478456a112d7dd7add7bb40
Basarat <basaratali@gmail.com>
2020-09-21 13:43:05 +1000

    wrap

---
+++ 33 optional/src/example.ts
@@ -0,0 +1,2 @@
+
+declare function someBooleanOrNullOrUndefined(): boolean | null | undefined;
---
+++ 33 optional/src/index.ts
@@ -0,0 +1,8 @@
+class Point {
+  x?: number;
+  y?: number;
+}
+
+const point = new Point();
+
+console.log(point.x); // undefined
