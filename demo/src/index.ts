e075507e34ddff8c6b1b064192af2234aeda6033
basarat <basaratali@gmail.com>
2024-06-07 17:13:23 +1000

    organize into advanced

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,17 +1,3 @@
-export const LoginMode = {
-  device: 'device',
-  email: 'email',
-  social: 'social',
-} as const;
-
-export type LoginMode = keyof typeof LoginMode;
-
-export function initiateLogin(mode: LoginMode) {
-  // ... 
-}
-
-initiateLogin('device');
-
-initiateLogin(LoginMode.device);
-
-Object.keys(LoginMode); // ['device', 'email', 'social']
+type A<T> = (x: T) => T;
+//p
+type B = <T>(x: T) => T;
